85b1e11ce93a3f5203f7f9400cd50747adeb48f5
Joe Shanahan <joe.shanahan@maginteractive.se>
2022-04-02 12:07:29 +0100

    Update EnemyChase.cs

--- Assets/Scripts/Enemy/EnemyChase.cs
+++ Assets/Scripts/Enemy/EnemyChase.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class EnemyChase : MonoBehaviour
 {
-    [ScriptableObject]
-    private Player player;
+    [SerializeField]
+    private PlayerMove _player;
     
     // Start is called before the first frame update
     void Start()
