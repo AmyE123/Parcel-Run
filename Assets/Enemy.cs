c1e61fd5ef342051b82299d63004fab03764393f
Joe Shanahan <joe.shanahan@maginteractive.se>
2022-04-02 19:25:56 +0100

    made enemies chase you only when seen, and they can dive now

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,13 +4,62 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public EnemyCalculatePath enemyCalculatePath;
+    [SerializeField]
+    private Player _chasingPlayer;
 
-    void Update()
+    [SerializeField]
+    private EnemyCalculatePath _pathCalculator;
+
+    [SerializeField]
+    private PersonMovement _movement;
+
+    [SerializeField]
+    private float _diveDistance;
+
+    private Vector3 _playerLastSeen;
+
+    void Start()
     {
-        if (enemyCalculatePath.HasEnemyCaughtPlayer())
+        StartCoroutine(EnemyAIRoutine());
+    }
+
+    private bool IsWithinDivingDistance()
+    {
+        Vector3 diff = _chasingPlayer.transform.position - transform.position;
+        diff.y = 0;
+        
+        return diff.magnitude <= _diveDistance;
+    }
+
+    IEnumerator EnemyAIRoutine()
+    {
+        while (true)
         {
-            Debug.Log("Caught player!");
+            if (_chasingPlayer == null)
+            {
+                yield return null;;
+                continue;
+            }
+
+            _pathCalculator.ChasePlayer(_chasingPlayer.transform.position);
+
+            if (IsWithinDivingDistance())
+            {
+                _movement.StartDive(_chasingPlayer.transform.position);
+
+                while (_movement.IsDiving)
+                    yield return new WaitForSeconds(0.1f);
+            }
+
+            yield return null;
         }
+    }
+
+    public void PlayerSeen(Player player)
+    {
+        if (_chasingPlayer != null)   
+            return;
+
+        _chasingPlayer = player;
     }
 }
---
+++ Assets/PlayerDetectionField.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetectionField : MonoBehaviour
+{
+    [SerializeField]
+    private Enemy _enemy;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            _enemy.PlayerSeen(other.GetComponent<Player>());
+    }
+}
