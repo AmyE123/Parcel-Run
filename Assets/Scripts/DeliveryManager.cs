88ae9375a8b58f7064c0d83e00667b3fb34c5e4d
Joe Shanahan <joe.shanahan@maginteractive.se>
2022-04-03 00:04:07 +0100

    improved randomisation of house picking

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -10,6 +10,8 @@ public class DeliveryManager : MonoBehaviour
     [SerializeField]
     private Postbox[] _allPostboxes;
 
+    private List<DeliveryHouse> _nextHouseList;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +20,26 @@ public class DeliveryManager : MonoBehaviour
         _allPostboxes = FindObjectsOfType<Postbox>();
 
         SpawnNewPackagesWhereNeeded();
+    }
 
-        StartCoroutine(GameLoop());
+    private DeliveryHouse GetNextHouse()
+    {
+        if (_nextHouseList == null || _nextHouseList.Count == 0)
+        {
+            _nextHouseList = new List<DeliveryHouse>(_allHouses);
+            Shuffle(_nextHouseList);
+        }
+
+        DeliveryHouse next = _nextHouseList[0];
+        _nextHouseList.RemoveAt(0);
+        return next;
     }
 
     public void DeliveryMade()
     {
         foreach (Postbox box in _allPostboxes)
             box.DeliveryMade();
-            
+
         SpawnNewPackagesWhereNeeded();
     }
 
@@ -37,16 +50,19 @@ public class DeliveryManager : MonoBehaviour
             if (box.IsReadyToSpawn == false)
                 continue;
 
-            box.SpawnPackage(_allHouses[0]);
+            box.SpawnPackage(GetNextHouse());
         }
     }
 
-
-    IEnumerator GameLoop()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(1f);
-        }
+    public static void Shuffle<T>(IList<T> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = Random.Range(0, n + 1);  
+            T value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
     }
 }
