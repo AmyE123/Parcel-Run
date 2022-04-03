5b7e0de8f31d7d962666691ce6066f95eb590468
JoePC <iam3djoe@gmail.com>
2022-04-03 19:18:44 +0100

    fix for missing anim events in cutscene

---
+++ Assets/Scripts/CutsceneAnimEvents.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneAnimEvents : MonoBehaviour
+{
+    public void DiveStopEvent()
+    {
+        
+    }
+
+    public void DiveRecoveredEvent()
+    {
+        
+    }
+}
